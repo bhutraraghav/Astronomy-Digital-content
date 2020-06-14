5bf2903d7cd4118215649fde7665f1be4a029f91
bhutraraghav <66239141+bhutraraghav@users.noreply.github.com>
2020-06-15 02:09:47 +0530

    Add files via upload

---
+++ Stars/js/cload.js
@@ -0,0 +1,74 @@
+var intro= document.getElementById("intro")
+       	fetch('text/intro.txt')
+  .then(response => response.text())
+  .then(text => intro.innerHTML=text);
+
+  var about1= document.getElementById("about1")
+       	fetch('text/about1.txt')
+  .then(response => response.text())
+  .then(text => about1.innerHTML=text);
+
+  var about2= document.getElementById("about2")
+       	fetch('text/about2.txt')
+  .then(response => response.text())
+  .then(text => about2.innerHTML=text);
+
+  var about3= document.getElementById("about3")
+       	fetch('text/about3.txt')
+  .then(response => response.text())
+  .then(text => about3.innerHTML=text);
+
+var st1= document.getElementById("st1")
+       	fetch('text/subtopic1.txt')
+  .then(response => response.text())
+  .then(text => st1.innerHTML=text);
+
+  var st2= document.getElementById("st2")
+       	fetch('text/subtopic2.txt')
+  .then(response => response.text())
+  .then(text => st2.innerHTML=text);
+
+ var st3= document.getElementById("st3")
+       	fetch('text/subtopic3.txt')
+  .then(response => response.text())
+  .then(text => st3.innerHTML=text);
+
+var st4= document.getElementById("st4")
+       	fetch('text/subtopic4.txt')
+  .then(response => response.text())
+  .then(text => st4.innerHTML=text);
+
+  var st5= document.getElementById("st5")
+       	fetch('text/subtopic5.txt')
+  .then(response => response.text())
+  .then(text => st5.innerHTML=text);
+
+  var st6= document.getElementById("st6")
+       	fetch('text/subtopic6.txt')
+  .then(response => response.text())
+  .then(text => st6.innerHTML=text);
+
+  var st7= document.getElementById("st7")
+       	fetch('text/subtopic7.txt')
+  .then(response => response.text())
+  .then(text => st7.innerHTML=text);
+
+  var st8= document.getElementById("st8")
+       	fetch('text/subtopic8.txt')
+  .then(response => response.text())
+  .then(text => st8.innerHTML=text);
+
+  var st9= document.getElementById("st9")
+       	fetch('text/subtopic9.txt')
+  .then(response => response.text())
+  .then(text => st9.innerHTML=text);
+
+  var st10= document.getElementById("st10")
+       	fetch('text/subtopic4.txt')
+  .then(response => response.text())
+  .then(text => st10.innerHTML=text);
+
+  var st11= document.getElementById("st11")
+       	fetch('text/subtopic11.txt')
+  .then(response => response.text())
+  .then(text => st11.innerHTML=text);
